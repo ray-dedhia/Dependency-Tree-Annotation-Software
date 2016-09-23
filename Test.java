b1a070d27abf15cab5b509a3d6ea3b1bf5700f65
ray-dedhia <rdedhia@mit.edu>
2016-09-23 05:06:42 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -17,9 +17,9 @@ public class Test {
 		//String[] words = {"Antidisestablishmentarianism", "I'm a triangle, but the sum of the measures of my angles is greater than 180 degrees because I lie on a hyperbolic plane.", "Are you coming to the tree?", "I ate two apples happily", "I'm going to Canada", "The bird flew", "There was a cat", "Ain't that true.", "Where are you from?", "I heard it on the grapevine", "Where are the snows of yesteryear?", "Come forth and have no fear", "I believe what I believe and what I believe is that what I believe is right", "She's the last true mouthpiece", "There has to be another way!", "This is the place where dreams are made", "May the odds be ever in your favor", "The crickets have arthritis", "Wouldn't that be like killing a mockingbird?"};        
 		//@SuppressWarnings("unused")
 		//DTE8 editor = 
-		new DTE9();//"/home/rheadedhia/Documents/POS.txt");
+		new DTE9();//"/home/Documents/POS.txt");
 		//editor.addSentences(words);
-		//editor.openConllFile("/home/rheadedhia/Documents/universal_treebanks_v2.0/std/en/en-universal-dev.conll");
+		//editor.openConllFile("/home/Documents/universal_treebanks_v2.0/std/en/en-universal-dev.conll");
 	}
 }
 
